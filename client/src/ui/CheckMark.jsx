256750bfeff01f82646fc74258ffbff29a15e302
smallobject <tiesti.os@gmail.com>
2021-03-22 23:21:36 +0200

    added new theme for tippyjs to follow our current theme

--- client/src/ui/CheckMark.jsx
+++ client/src/ui/CheckMark.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ReactComponent as CheckMarkSVG } from '../assets/check-mark.svg';
 import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
 import './ui.css';
 
 const Example = () => {
   return (
     <div>
-      <Tippy content='hello'>
-        <CheckMarkSVG className='cursor-pointer' />
+      <Tippy content='Item is verified' className='text-xs' theme='tomato'>
+        <CheckMarkSVG className='cursor-pointer w-2.5 h-2.5 mt-1.5 ml-1.5' />
       </Tippy>
     </div>
   );
